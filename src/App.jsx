258482e8d2d37aede700e655269fba3925d0f6f2
Alvina <alvina@tavroxgames.com>
2023-07-23 23:36:51 +0200

    added: rapier + suzanne

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Car from "./Car";
 
 export default function App() {
   return (
     <Canvas>
       <mesh>
         <boxGeometry />
-        <meshBasicMaterial color='darkblue' />
+        <meshBasicMaterial color="darkblue" />
       </mesh>
+      <Car />
       <OrbitControls />
     </Canvas>
   );
