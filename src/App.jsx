9b67f2c6dbb2cdf2d24b86b36b1c923afa53c047
Alvina <alvina@tavroxgames.com>
2023-07-24 21:39:35 +0200

    added: perf + car movement

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { KeyboardControls, OrbitControls } from "@react-three/drei";
+import { Physics, RigidBody } from "@react-three/rapier";
 import Car from "./Car";
+import { Perf } from "r3f-perf";
 
 export default function App() {
   return (
-    <Canvas>
-      <mesh>
-        <boxGeometry />
-        <meshBasicMaterial color="darkblue" />
-      </mesh>
-      <Car />
-      <OrbitControls />
-    </Canvas>
+    <KeyboardControls
+      map={[
+        // { name: "forward", keys: ["ArrowUp", "KeyW"] },
+        // { name: "backward", keys: ["ArrowDown", "KeyS"] },
+        { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+        { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+        // { name: "jump", keys: ["Space"] },
+      ]}
+    >
+      {" "}
+      <Canvas shadows>
+        <Perf />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          {" "}
+          <RigidBody type="fixed">
+            <mesh position={[0, -0.1, 0]} receiveShadow>
+              <boxGeometry args={[200, 0.2, 200]} />
+              <meshPhysicalMaterial color="darkblue" />
+            </mesh>
+          </RigidBody>
+          <Car castShadow></Car>
+        </Physics>
+        {/* <OrbitControls /> */}
+      </Canvas>
+    </KeyboardControls>
   );
 }
--- src/Car.jsx
+++ src/Car.jsx
@@ -1,18 +1,80 @@
+import * as THREE from "three";
 import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, useKeyboardControls } from "@react-three/drei";
+import { RigidBody } from "@react-three/rapier";
+import { useFrame } from "@react-three/fiber";
 
 export default function Car(props) {
+  const body = useRef();
   const { nodes, materials } = useGLTF("/suzanne.gltf");
+  const [subscribeKeys, getKeys] = useKeyboardControls();
+  const cameraPosition = new THREE.Vector3();
+  const cameraTarget = new THREE.Vector3();
+  // const pointLight = useRef();
 
+  useFrame((state, delta) => {
+    const { leftward, rightward } = getKeys();
+    const impulse = { x: 0, y: 0, z: 0 };
+    const impulseStrength = 1 * delta;
+
+    // impulse.z -= impulseStrength;
+    if (impulse.z < 1) {
+      impulse.z -= impulseStrength;
+    }
+    if (leftward) {
+      impulse.x -= impulseStrength * 2;
+    }
+    if (rightward) {
+      impulse.x += impulseStrength * 2;
+    }
+    // console.log(delta);
+    // body.current.applyImpulse({ x: 0, y: 0, z: 0 });
+    body.current.applyImpulse(impulse);
+    // body.current.translation().z -= impulseStrength;
+
+    const bodyPosition = body.current.translation();
+    //CAMERA
+    // const cameraPosition = new THREE.Vector3();
+    cameraPosition.copy(bodyPosition);
+    cameraPosition.z += 2.25;
+    cameraPosition.y += 2;
+    // const cameraTarget = new THREE.Vector3();
+    cameraTarget.copy(bodyPosition);
+    cameraTarget.y += 0.5;
+    console.log();
+    state.camera.position.copy(cameraPosition);
+    state.camera.lookAt(cameraTarget);
+
+    // pointLight follow along
+    // pointLight.current.position.z = body.current.position.z + 1;
+    // pointLight.current.position.x = body.current.position.x;
+    // pointLight.current.position.y -= slowSine / 80;
+  });
   return (
     <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Suzanne.geometry}
-        material={nodes.Suzanne.material}
-        position={[0, 0.189, -0.043]}
-      />
+      <RigidBody ref={body}>
+        {" "}
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Suzanne.geometry}
+          material={nodes.Suzanne.material}
+          position={[0, 1, 0]}
+          scale={[0.25, 0.25, 0.25]}
+        />
+        <directionalLight
+          castShadow
+          position={[4, 4, 1]}
+          intensity={1.5}
+          shadow-mapSize={[1024, 1024]}
+          shadow-camera-near={1}
+          shadow-camera-far={10}
+          shadow-camera-top={10}
+          shadow-camera-right={10}
+          shadow-camera-bottom={-10}
+          shadow-camera-left={-10}
+        />
+      </RigidBody>
     </group>
   );
 }
